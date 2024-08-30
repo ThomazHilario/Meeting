62e3b12d41ba7a1694e28e10f62f3c55cbacdb7d
Thomaz Alves <thomazhilario5@gmail.com>
2024-08-30 10:33:57 -0300

    feat(fornecedores.tsx): Exibir informações do fornecedor
    - Criar um modal para exibir as informações do fornecedor.

--- app/fornecedores.tsx
+++ app/fornecedores.tsx
@@ -2,7 +2,9 @@
 import { useEffect, useState } from 'react'
 
 // React native
-import { View, Text, StyleSheet, TextInput, FlatList } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Image, FlatList, Modal, Pressable } from 'react-native'
+
+import { Ionicons } from '@expo/vector-icons'
 
 // Picker
 import { Picker } from '@react-native-picker/picker'
@@ -21,6 +23,9 @@ interface FornecedorProps{
 }
 
 export default function Fornecedores(){
+
+    // state - isLoading
+    const [isLoading, setIsLoading] = useState<boolean>(true)
 
     useEffect(() => {
         async function getFonecedores(){
@@ -42,6 +47,8 @@ export default function Fornecedores(){
 
                 // Salvando na state de Fornecedores
                 setFornecedores(fornecedoresDatabase)
+
+                setIsLoading(false)
             } catch (error) {
                 console.log(error)
             }
@@ -52,7 +59,11 @@ export default function Fornecedores(){
 
     }, [])
 
+    // state - fornecedores
     const [fornecedores, setFornecedores] = useState<FornecedorProps[]>()
+
+    // state - isVisible
+    const [isVisible, setIsVisible] = useState<boolean>(false)
 
     return(
         <View style={style.container}>
@@ -67,14 +78,46 @@ export default function Fornecedores(){
             </View>
 
             <View style={style.listContainer}>
-                <FlatList
-                    data={fornecedores}
-                    renderItem={(item) => (
-                        <View style={style.fornecedorStyle}>
-                            {item.item.nome}
-                        </View>
-                    )}
-                />
+                {isLoading ? (
+                    <Text style={style.title}>Buscando Fornecedores...</Text>
+                ):(
+                    <FlatList
+                        data={fornecedores}
+                        renderItem={(item) => (
+                            <View style={style.fornecedorStyle}>
+
+                                {/* Content */}
+                                <View style={style.fornecedorContent}>
+                                    <Image style={style.imagem} source={{uri:item.item.imagem as string}}/>
+                                    <Text>{item.item.nome}</Text>
+                                </View>
+
+                                {/* Button open modal */}
+                                <Pressable onPress={() => setIsVisible(true)}>
+                                    <Ionicons name='open' color='black' size={24}/>
+                                </Pressable>
+
+                                {/* Modal */}
+                                <Modal animationType='fade' transparent={true} visible={isVisible}>
+                                    <View style={style.overlay}>
+                                        <View style={style.modal}>
+                                            <Pressable style={{alignSelf:'flex-end'}} onPress={() => setIsVisible(false)}>
+                                                <Ionicons name='close' color='white' size={24}/>
+                                            </Pressable>
+
+                                            <Image style={style.imageModal} source={{uri:item.item.imagem as string}}/>
+
+                                            <Text style={style.text}>Nome: {item.item.nome}</Text>
+                                            <Text style={style.text}>Endereco: {item.item.endereco}</Text>
+                                            <Text style={style.text}>Contato: {item.item.contato}</Text>
+                                            <Text style={style.text}>Categoria: {item.item.categoria}</Text>
+                                        </View>
+                                    </View>
+                                </Modal>
+                            </View>
+                        )}
+                    />
+                )}
             </View>
         </View>
     )
@@ -108,7 +151,15 @@ const style = StyleSheet.create({
     },
 
     text:{
-        color:'rgb(255, 255, 255)'
+        color:'rgb(255, 255, 255)',
+        paddingLeft:10,
+        fontSize:17
+    },
+
+    title:{
+        textAlign:'center',
+        color:'white',
+        fontSize:20
     },
 
     listContainer:{
@@ -117,9 +168,47 @@ const style = StyleSheet.create({
     },
 
     fornecedorStyle:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
         backgroundColor:'rgb(242, 234, 234)',
         padding:10,
         borderRadius:5,
         margin:5
-    }
+    },
+
+    fornecedorContent:{
+        flexDirection:'row',
+        alignItems:'center',
+        gap:10
+    },
+    
+    imagem:{
+        height:40,
+        width:40,
+        borderRadius:100
+    },
+
+    modal:{
+        backgroundColor:'rgb(50, 50, 50)',
+        height:'50%',
+        width:'90%',
+        transform:'translate(-50%, 30%)',
+        left:'50%',
+        padding:20,
+        borderRadius:10,
+        gap:10
+    },
+
+    overlay:{
+        backgroundColor:'rgba(36, 36, 36, 0.4)',
+        height:'100%',
+    },
+
+    imageModal:{
+        height:100,
+        width:100,
+        borderRadius:100,
+        alignSelf:'center'
+    },
 })
